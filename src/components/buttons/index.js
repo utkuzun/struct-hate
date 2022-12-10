86aba794d8e2cacd0c0db3d3b37800d076e5ccb8
utkuzun <utkuzun50@gmail.com>
2022-12-11 01:57:30 +0300

    buttons intro

--- src/components/buttons/index.js
+++ src/components/buttons/index.js
@@ -5,7 +5,7 @@ import styles from './Button.module.css'
 const MyButton = ({ text }) => {
   return (
     <div className={styles.container}>
-      <div className={styles.btn}>
+      <div className={`${styles.btn} ${styles.color1}`}>
         <a href='#'>{text}</a>
       </div>
     </div>
