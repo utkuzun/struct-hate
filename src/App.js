00d3de5003a109d68985cbe294c5e109a7cfb68e
utkuzun <utkuzun50@gmail.com>
2022-12-11 19:48:57 +0300

    bg in swipper adjust

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from 'react'
 import './App.css'
 
-import styles from './components/buttons/Button.module.css'
+// import styles from './components/buttons/Button.module.css'
 
 import Header from './components/header'
 import NFTCarousel from './components/NFT carousel'
 import Team from './components/team'
 import Footer from './components/footer'
-import MyButton from './components/buttons'
+// import MyButton from './components/buttons'
 function App() {
   return (
     <div className='App'>
       <Header />
-      <div className={styles.container}>
+      {/* <div className={styles.container}>
         <MyButton text={'Start App'} color={'color1'} />
         <MyButton text={'Connect'} color={'color2'} />
-      </div>
+      </div> */}
       <NFTCarousel />
       <Team />
       <Footer />
